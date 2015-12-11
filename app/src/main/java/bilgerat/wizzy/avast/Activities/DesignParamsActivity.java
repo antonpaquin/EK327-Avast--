acb4d4db4a1dd39d1989edf28090084b1666a51a
Anton Paquin <antonpaquin@gmail.com>
2015-12-11 03:40:32 -0500

    Let's try again git

--- app/src/main/java/bilgerat/wizzy/avast/Activities/DesignParamsActivity.java
+++ app/src/main/java/bilgerat/wizzy/avast/Activities/DesignParamsActivity.java
@@ -28,26 +28,4 @@ public class DesignParamsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design_params);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_design_params, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/bilgerat/wizzy/avast/Support/VirusView.java
+++ app/src/main/java/bilgerat/wizzy/avast/Support/VirusView.java
@@ -1,14 +1,36 @@
 package bilgerat.wizzy.avast.Support;
 
+import android.content.Context;
 import android.view.View;
 
 /**
  * Created by Anton on 12/6/2015.
  *
  */
-public class VirusView {
+public class VirusView extends View{
     /*
      * Extends view
      * Custom class for showing virus images
      */
+
+    private int[][] colors = new int[13][3];
+
+    public VirusView(Context context) {
+        super(context);
+    }
+
+    private void greyColors() {
+        for (int i=0; i<colors.length; i++) {
+            for (int j=0; j<colors[0].length; j++) {
+                colors[i][j] = 128;
+            }
+        }
+    }
+
+    private void setColor(int index, int r, int g, int b) {
+        colors[index] = new int[] {r,g,b};
+        
+    }
+
+
 }
